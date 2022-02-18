d2846ba7e08343f2c6bbd87e3aaf50f3dc9dd562
root <root@project-dev.pensoft.local>
2022-02-18 11:49:10 +0200

    migration file

---
+++ updates/builder_table_update_pensoft_eventsextension_attendee_answer_2.php
@@ -0,0 +1,23 @@
+<?php namespace Pensoft\Eventsextension\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftEventsextensionAttendeeAnswer2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_eventsextension_attendee_answer', function($table)
+        {
+            $table->text('answer')->nullable(false)->unsigned(false)->default(null)->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_eventsextension_attendee_answer', function($table)
+        {
+            $table->string('answer', 255)->nullable(false)->unsigned(false)->default(null)->change();
+        });
+    }
+}
